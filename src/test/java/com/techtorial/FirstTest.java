48c7639ab06d76dbb4713a657b248a442902b784
NARGIZA001 <nkeneshbaeva1@gmail.com>
2020-01-11 16:34:41 -0600

    jenkins

---
+++ src/test/java/com/techtorial/FirstTest.java
@@ -0,0 +1,12 @@
+package com.techtorial;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void secondTest(){
+
+        System.out.println("Jenkis Intro from Nargiza");
+    }
+}
